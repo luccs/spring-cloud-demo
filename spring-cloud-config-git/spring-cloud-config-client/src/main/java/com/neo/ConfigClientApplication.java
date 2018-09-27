058d3e08d02ecdc5660c52e4dc07d226464a0fc5
luccs624061082@163.com <luccs624061082@163.com>
2018-09-27 11:14:11 +0800

    spring clould config 高可用提交

--- spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/neo/ConfigClientApplication.java
+++ spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/neo/ConfigClientApplication.java
@@ -1,9 +1,10 @@
 package com.neo;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigClientApplication {
 
