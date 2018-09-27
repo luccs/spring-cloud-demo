eb8803f4474a8ba1f194a52b78394f9aeda5e6de
luccs624061082@163.com <luccs624061082@163.com>
2018-09-27 16:00:14 +0800

    spring cloud bus /bus/refresh 更新配置信息

--- spring-cloud-config-eureka-bus/spring-cloud-config-server/src/main/java/com/neo/ConfigServerApplication.java
+++ spring-cloud-config-eureka-bus/spring-cloud-config-server/src/main/java/com/neo/ConfigServerApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
+/**
+ * 使用
+ *  curl -X POST http://localhost:8002/bus/refresh 更新配置信息
+ * @author luccs
+ *
+ */
 @EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
